849ebcc76171928320bd608fd7ec66a3cc7a148c
pancake423 <dibbybucklenuggets@gmail.com>
2025-01-16 14:13:30 -0500

    added spritesheets
    new module to create sprite sheets out of the card faces, ready to be loaded into webgl.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,27 +1,27 @@
-import * as cr from "/scripts/card-renderer.js";
+import * as ss from "/scripts/spritesheet.js";
 import { mat3, mat4 } from "/gl-matrix/index.js"; // for future reference
 
 var c;
 var ctx;
 
-console.log("1" in cr.FACE_VALUES);
-
 window.onload = () => {
   c = document.getElementById("c");
   ctx = c.getContext("2d");
 
-  ctx.drawImage(cr.drawCardFace("hearts", "A"), 16, 16);
-  ctx.drawImage(cr.drawCardFace("diamonds", "10"), 32 + cr.CARD_W, 16);
-  ctx.drawImage(cr.drawCardFace("clubs", "A"), 16, 32 + cr.CARD_H);
-  ctx.drawImage(
-    cr.drawCardFace("spades", "10"),
-    32 + cr.CARD_W,
-    32 + cr.CARD_H,
-  );
-  ctx.drawImage(
-    cr.drawCardBack("red", "classic"),
-    48 + cr.CARD_W * 2,
-    32 + cr.CARD_H,
-  );
-  ctx.drawImage(cr.drawCardBack("blue", "classic"), 48 + cr.CARD_W * 2, 16);
+  const spriteSheets = ss.makeSpriteSheets([
+    ["blue", "classic"],
+    ["red", "classic"],
+  ]);
+  console.log(ss.getFaceLoc("hearts", "3"));
+  console.log(ss.getBackLoc(2));
+
+  for (let i = 0; i < spriteSheets.length; i++) {
+    ctx.drawImage(
+      spriteSheets[i],
+      (spriteSheets[i].width / 2) * Math.floor(i / 2),
+      (spriteSheets[i].height / 2) * (i % 2),
+      spriteSheets[i].width / 2,
+      spriteSheets[i].height / 2,
+    );
+  }
 };
---
+++ public/scripts/spritesheet.js
@@ -0,0 +1,91 @@
+/*
+make sprite sheets out of all of the possible card faces we need for the game.
+also maintains a lookup of where to find each card face.
+
+There are four 5x3 spritesheets.
+There is space for up to 60 card faces- 52 for the fronts, and up to 8 players.
+*/
+import * as cr from "/scripts/card-renderer.js";
+
+let CARD_LOOKUP = {};
+const SHEET_DIMENSIONS = [5, 3]; // 5 wide, 3 high (in cards)
+const N_SHEETS = 4;
+let SHEETS = [];
+let SHEETS_CTX = [];
+let idx = [0, 0, 0];
+/*
+initialize sprite sheets and add the 52 standard card faces.
+
+playerCards is an array of [[color, design], ]
+NOTE: must have 8 items or less, or this will break
+*/
+function makeSpriteSheets(playerCards) {
+  // reset sprite sheets (in case of new games)
+  idx = [0, 0, 0];
+  CARD_LOOKUP = {};
+  SHEETS = [];
+  SHEETS_CTX = [];
+
+  // initialize empty sprite sheets
+  for (let i = 0; i < N_SHEETS; i++) {
+    const c = new OffscreenCanvas(
+      cr.CARD_W * SHEET_DIMENSIONS[0],
+      cr.CARD_H * SHEET_DIMENSIONS[1],
+    );
+    SHEETS.push(c);
+    SHEETS_CTX.push(c.getContext("2d"));
+  }
+  // add standard cards to spritesheets
+  for (const suit of cr.FACE_SUITS) {
+    for (const value of cr.FACE_VALUES) {
+      addImageToSheet(cr.drawCardFace(suit, value), suit[0] + value);
+    }
+  }
+  // add player cards to spritesheets
+  for (let i = 0; i < playerCards.length; i++) {
+    const [color, design] = playerCards[i];
+    addImageToSheet(cr.drawCardBack(color, design), "p" + i);
+  }
+
+  // return spritesheets as a list of image bitmaps.
+  return SHEETS.map((c) => c.transferToImageBitmap());
+}
+
+/*
+adds a new card image to the correct position in the spritesheet,
+adds it to the card lookup table, and increments the spritesheet position.
+*/
+function addImageToSheet(img, loc) {
+  const ctx = SHEETS_CTX[idx[0]];
+  ctx.drawImage(img, idx[1] * cr.CARD_W, idx[2] * cr.CARD_H);
+  CARD_LOOKUP[loc] = idx;
+  idx = nextIndex(idx);
+}
+
+/*
+idx is a list of [sheet, x, y].
+returns the new index of the next valid location.
+*/
+function nextIndex(idx) {
+  let [sheet, x, y] = idx;
+  y++;
+  if (y >= SHEET_DIMENSIONS[1]) {
+    y = 0;
+    x++;
+  }
+  if (x >= SHEET_DIMENSIONS[0]) {
+    x = 0;
+    sheet++;
+  }
+  return [sheet, x, y];
+}
+
+function getFaceLoc(suit, value) {
+  return CARD_LOOKUP[suit[0] + value];
+}
+
+function getBackLoc(playerID) {
+  return CARD_LOOKUP["p" + playerID];
+}
+
+export { makeSpriteSheets, getFaceLoc, getBackLoc };
